dc7c80ce57f43b2edf7444f508b84a8b34e70b6d
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2025-12-15 19:43:35 -0300

    * Defined Order Status Emails structure     * /emails/status/index.ts         * Defines OrderStatusEmails class to common export all the different status emails:             - CancelledOrder.ts             - PendingOrder.ts             - SentOrder.ts             - DeliveredOrder.ts
    * Created routers for each model for the future:
        - adminRouter
        - orderRouter
        - productRouter
        - paymentRouter (next to be worked on)

---
+++ src/emails/status/DeliveredOrder.ts
@@ -0,0 +1,26 @@
+import resend from "../../config/resend";
+import { InternalServerError } from "../../errors/server-error";
+import { OrderInterface } from "../../models/Order";
+import { UserInterface } from "../../models/User";
+
+export class DeliveredOrderEmail {
+    static sendDeliveredOrderEmail = async (user: UserInterface, order: OrderInterface) => { 
+        try {
+            const emailHTML = ``;
+
+            const mailOptions = {
+                from: `"Morango Joyas" <${process.env.NOREPLY_EMAIL}>`,
+                to: [user.email], 
+                subject: `🎉✅ Orden Entregada - ${user.name}`, 
+                html: emailHTML
+            }
+
+            const response = await resend.emails.send(mailOptions); 
+            console.log("✅ Email sent successfully", user.email);
+            console.log(response)
+        } catch (error) {
+            console.error("❌ Error sending email:", error);
+            throw new InternalServerError(); 
+        }
+    }
+}
