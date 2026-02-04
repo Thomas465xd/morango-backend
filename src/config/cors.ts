5c565f9a94c049c9e3624a38faca7901b0b8910d
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-02-04 14:05:00 -0300

    * CORS config fix to accept request from www.morangojoyas.cl origin

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,11 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL, "https://morangojoyas.cl"];
+        const whitelist = [
+            process.env.FRONTEND_URL, 
+            "https://morangojoyas.cl", 
+            "https://www.morangojoyas.cl"
+        ];
 
         // Allow requests with no origin (e.g., Postman, cURL)
         if (!origin || whitelist.includes(origin)) {
